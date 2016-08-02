7ca6bd5f7557acbbfbbf74c5b7265add55c25ff2
Stuart Littlefair <s.littlefair@shef.ac.uk>
2016-08-02 13:53:08 +0100

    added setupy.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from __future__ import (print_function, absolute_import)
+from setuptools import setup
+
+setup(
+    name = "gp_doppler",
+    version="0.1",
+    description="Doppler Imaging using Gaussian Processes for penalising complexity",
+    author_email = "s.littlefair@shef.ac.uk",
+    packages = ['gp_doppler'],
+    zip_safe=False
+)
